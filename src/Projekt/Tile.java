ebf311a238bd39af4dad573820d4551919c4d9a4
Hibeas <79516378+Hibeas@users.noreply.github.com>
2021-03-14 17:35:33 +0100

    bomby+normlne - odkrywanie za pomocą boolean

--- src/Projekt/Tile.java
+++ src/Projekt/Tile.java
@@ -4,25 +4,69 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Tile {
-    private BufferedImage image;
+    private BufferedImage normal;
+    private BufferedImage openedImage;
+    private BufferedImage flag;
+    private BufferedImage bombImage;
+
     private int x;
     private int y;
+    private boolean bomb;
+    private boolean opened;
+    private int amoundOFnearBombs;
 
     private static int width = frame.getScreenWidth()/World.getWidth();
     private static int hight = frame.getScreenHight()/World.getHight();
 
-    public Tile(int x, int y){
+    public Tile(int x, int y, BufferedImage normal, BufferedImage bomb, BufferedImage openedImage, BufferedImage flag){
         this.x=x;
         this.y=y;//tworzenie obiektu kafelek
+        this.normal = normal;
+        this.bombImage = bomb;
+        this.openedImage = openedImage;
+        this.flag = flag;
+
+        opened = true;
     }
 
-    public void setImage(BufferedImage image) {
-        this.image = image;
+    public void setNormal(BufferedImage normal) {
+        this.normal = normal;
+    }
+    public void setOpenedImage(BufferedImage openedImage) {
+        this.openedImage = openedImage;
+    }
+
+    public void setOpened(boolean opened) {
+        this.opened = opened;
+    }
+
+    public boolean isOpened() {
+        return opened;
+    }
+
+
+
+    public void setBomb(boolean bomb) {
+        this.bomb = bomb;
+    }
+
+    public boolean isBomb() {
+        return bomb;
+    }
+
+    public void setAmoundOFnearBombs(int amoundOFnearBombs) {
+        this.amoundOFnearBombs = amoundOFnearBombs;
     }
 
     //ta klasa odpowiada za kafelki
     public void draw(Graphics g){
-        g.drawImage(image, x*width, y*width, null);
+        if(!opened)g.drawImage(normal, x*width, y*width, null);
+        else {
+            if(bomb)g.drawImage(bombImage, x*width, y*width, null);
+            else {
+                g.drawImage(openedImage, x*width, y*width, null);
+            }
+        }
     }
 
     public static int getWidth() {
--- src/Projekt/World.java
+++ src/Projekt/World.java
@@ -2,11 +2,15 @@ package Projekt;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class World {
     //"swiat" sapera
-    private static int width = 40;
-    private static int hight = 40;
+    private static int width = 20;//tutaj możemy zmieniąc wielkość, a dzięki temu zarazem ilość kafelków
+    private static int hight = 20;
+
+    private final int AMOUNT_OF_BOMBS = 20;//tutaj ustawiamy ilość bomb
+    private Random random;
 
     private Tile[][] tiles;
 
@@ -14,15 +18,31 @@ public class World {
     private BufferedImage flag = ImageLoader.scale(ImageLoader.loadImage("grafika/flag.png"), Tile.getWidth(), Tile.getHight());
     private BufferedImage normal = ImageLoader.scale(ImageLoader.loadImage("grafika/normal.png"), Tile.getWidth(), Tile.getHight());
     private BufferedImage pressed = ImageLoader.scale(ImageLoader.loadImage("grafika/pressed.png"), Tile.getWidth(), Tile.getHight());
-    public World(){
+    public World() {
+        random = new Random();
+
         tiles = new Tile[width][hight];//tworzenie kafelka
 
-        for(int x=0;x<width;x++){
-            for(int y=0;y<hight;y++){
-                tiles[x][y]=new Tile(x,y);
-                tiles[x][y].setImage(normal);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < hight; y++) {
+                tiles[x][y] = new Tile(x, y, normal, bomb, pressed, flag);
             }
         }
+        placeBombs();
+    }
+    private void placeBombs(){
+        for (int i=0;i<AMOUNT_OF_BOMBS;i++){
+            placBomb();
+        }
+    }
+    private void placBomb(){
+        int x = random.nextInt(width);
+        int y = random.nextInt(hight);
+
+        if(!tiles[x][y].isBomb()){//kiedy nie ma bomby to ...
+            tiles[x][y].setBomb(true);
+        }
+        else placBomb();
     }
     public void draw(Graphics g){
         for(int x=0;x<width;x++) {
